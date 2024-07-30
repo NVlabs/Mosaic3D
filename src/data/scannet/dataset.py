a06c565019daf0a986457f92b3444f3a28b7b69b
Junha Lee <junhakiwi@postech.ac.kr>
2024-07-29 17:26:38 -0700

    wip: train on our dataset

---
+++ src/data/scannet/dataset.py
@@ -0,0 +1,183 @@
+import glob
+import os
+from pathlib import Path
+from typing import Dict, List, Optional
+
+import numpy as np
+import torch
+from natsort import natsorted
+from omegaconf import OmegaConf
+from torch.utils.data import Dataset
+
+from src.data.metadata.scannet import (
+    CLASS_LABELS_20,
+    CLASS_LABELS_200,
+    VALID_CLASS_IDS_20,
+    VALID_CLASS_IDS_200,
+)
+from src.data.transform import Compose
+from src.utils import RankedLogger
+
+log = RankedLogger(__name__, rank_zero_only=False)
+
+
+class ScanNetDataset(Dataset):
+    CLASS_LABELS = CLASS_LABELS_20
+    CLASS_IDS = VALID_CLASS_IDS_20
+
+    def __init__(
+        self,
+        data_dir: str,
+        caption_dir: str,
+        split: str,
+        transforms: None,
+        view_sample_ratio: Optional[float] = None,
+        base_class_idx: Optional[List[int]] = None,
+        novel_class_idx: Optional[List[int]] = None,
+        ignore_class_idx: Optional[List[int]] = None,
+        ignore_label: int = -100,
+    ):
+        super().__init__()
+        self.data_dir = Path(data_dir)
+        self.caption_dir = Path(caption_dir)
+        self.split = split
+        self.view_sample_ratio = view_sample_ratio
+        self.class_names = self.CLASS_LABELS
+
+        # read scene names for split
+        with open(f"src/data/metadata/split_files/scannetv2_{self.split}.txt") as f:
+            self.scene_names = natsorted([line.strip() for line in f.readlines()])
+
+        # class label mappers
+        self.valid_class_idx = np.arange(len(self.CLASS_LABELS)).tolist()
+        if base_class_idx is not None:
+            self.base_class_mapper = self.build_class_mapper(base_class_idx, ignore_label)
+            self.binary_class_mapper = self.build_binary_class_mapper(
+                base_class_idx, novel_class_idx, ignore_class_idx, ignore_label
+            )
+        if ignore_class_idx is not None:
+            for c in ignore_class_idx:
+                self.valid_class_idx.remove(c)
+        self.valid_class_mapper = self.build_class_mapper(
+            self.valid_class_idx, ignore_label, squeeze_label=self.split == "train"
+        )
+        self.ignore_label = ignore_label
+
+        # data transform
+        self.transforms = Compose(OmegaConf.to_container(transforms))
+
+        log.info(f"Loaded {self.__len__()} samples in {self.split} set.")
+
+        # backward compatibility for regionplc
+        self.caption_cfg = dict(GATHER_CAPTION=False)
+
+    @staticmethod
+    def build_class_mapper(class_idx, ignore_label, squeeze_label=True):
+        remapper = np.ones(256, dtype=np.int64) * ignore_label
+        for i, x in enumerate(class_idx):
+            if squeeze_label:
+                remapper[x] = i
+            else:
+                remapper[x] = x
+        return remapper
+
+    @staticmethod
+    def build_binary_class_mapper(base_class_idx, novel_class_idx, ignore_class_idx, ignore_label):
+        remapper = np.ones(256, dtype=np.int64) * ignore_label  # base: 1, novel: 0
+        for _, x in enumerate(base_class_idx):
+            remapper[x] = 1
+        for _, x in enumerate(novel_class_idx):
+            remapper[x] = 0
+        # ignored categories are mapped to novel
+        for _, x in enumerate(ignore_class_idx):
+            remapper[x] = 0
+        return remapper
+
+    def __len__(self):
+        return len(self.scene_names)
+
+    def load_caption(self, scene_name):
+        caption_files = natsorted(glob.glob(os.path.join(self.caption_dir, f"{scene_name}*")))
+        if self.view_sample_ratio < 1.0:
+            caption_files = np.random.choice(
+                caption_files,
+                max(1, int(len(caption_files) * self.view_sample_ratio)),
+                replace=False,
+            )
+        caption_list = [np.load(file) for file in caption_files]
+
+        caption_idx, caption_text = [], []
+        for caption in caption_list:
+            point_indices_flatten = caption["point_indices"]
+            num_points = caption["num_points"]
+            cumsum = np.cumsum(num_points)[:-1]
+            point_indices = np.split(point_indices_flatten, cumsum)
+            caption_idx.extend([torch.from_numpy(indices).int() for indices in point_indices])
+            caption_text.extend(caption["captions"])
+        return caption_idx, caption_text
+
+    def __getitem__(self, idx):
+        scene_name = self.scene_names[idx]
+        scene_dir = self.data_dir / self.split / scene_name
+
+        # load pcd data
+        coord = np.load(scene_dir / "coord.npy")
+        color = np.load(scene_dir / "color.npy")
+        segment = np.load(scene_dir / "segment20.npy")
+        instance = np.load(scene_dir / "instance.npy")
+
+        # load
+        # normalize rgb to [0,255]
+        color = np.clip((color + 1.0) * 127.5, 0, 255)
+
+        # class mapping
+        semantic_label = self.valid_class_mapper[segment.astype(np.int64)]
+        instance[segment == self.ignore_label] = self.ignore_label
+        binary_label = np.ones_like(segment)
+        if hasattr(self, "base_class_mapper"):
+            binary_label = self.binary_class_mapper[segment.astype(np.int64)].astype(np.float32)
+
+        data_dict = dict(
+            coord=coord,
+            color=color,
+            segment=semantic_label,
+            instance=instance,
+            binary=binary_label,
+            origin_idx=np.arange(coord.shape[0]).astype(np.int64),
+            pc_count=len(coord),
+        )
+
+        # load captions
+        if self.split == "train":
+            caption_idx, caption_text = self.load_caption(scene_name)
+            data_dict["caption_data"] = {"idx": caption_idx, "caption": caption_text}
+
+        data_dict = self.transforms(data_dict)
+        return data_dict
+
+
+class ScanNet200Dataset(ScanNetDataset):
+    CLASS_LABELS = CLASS_LABELS_200
+    CLASS_IDS = VALID_CLASS_IDS_200
+
+    def __init__(
+        self,
+        data_dir: str,
+        caption_dir: str,
+        split: str,
+        transforms: None,
+        base_class_idx: Optional[List[int]],
+        novel_class_idx: Optional[List[int]],
+        ignore_class_idx: Optional[List[int]],
+        ignore_label: int = -100,
+    ):
+        super().__init__(
+            data_dir,
+            caption_dir,
+            split,
+            transforms,
+            base_class_idx,
+            novel_class_idx,
+            ignore_class_idx,
+            ignore_label,
+        )
