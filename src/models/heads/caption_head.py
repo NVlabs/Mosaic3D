10f8300da27387ca2637baddd06a187573d1969f
Christopher Choy <cchoy@nvidia.com>
2024-08-13 14:25:23 -0700

    CSR based caption head ROI pooling

--- src/models/heads/caption_head.py
+++ src/models/heads/caption_head.py
@@ -1,182 +1,90 @@
+from typing import Dict, List, Optional, Tuple
+
 import numpy as np
 import torch
 import torch.nn as nn
-from torch_scatter import scatter
+from jaxtyping import Int
+from torch import Tensor
+from torch_scatter import segment_csr
+from warp.convnet.geometry.point_collection import PointCollection
 
 from src.models.heads.head_base import BaseHead
-from src.models.regionplc.utils.fp16 import force_fp32
+
+
+# Convert List[List[Tensor]] to concatenated Tensor, and offsets
+def convert_list_list_tensor_to_tensor(
+    list_list_tensor: List[List[Int[Tensor, "N"]]],  # noqa: F722, F821
+    batch_offset: Optional[Int[Tensor, "B + 1"]] = None,  # noqa: F722, F821
+    remove_empty_list: bool = True,
+) -> Tuple[Int[Tensor, "L"], Int[Tensor, "M + 1"], Int[Tensor, "M"]]:  # noqa: F722, F821
+    # Concatenate inner lists first and generate offsets for the inner lists
+    counts = [[len(l) for l in list_tensor] for list_tensor in list_list_tensor]  # noqa: E741
+    list_tensor = [torch.cat(l, 0) for l in list_list_tensor]  # noqa: E741
+    # Add batch offset if provided
+    if batch_offset is not None:
+        list_tensor = [l + batch_offset[i] for i, l in enumerate(list_tensor)]
+    else:
+        assert len(list_tensor) == 1, "batch_offset must be provided if len(list_tensor) > 1"
+
+    # Concatenate all lists and generate offsets for the outer lists
+    tensor = torch.cat(list_tensor, 0)
+    counts_flat = [c for sublist in counts for c in sublist]
+    cumsum = np.cumsum(counts_flat)
+    non_empty = np.array(counts_flat) > 0
+
+    if remove_empty_list:
+        offsets = torch.tensor([0] + cumsum[non_empty].tolist())
+    else:
+        offsets = torch.tensor([0] + cumsum.tolist())
+
+    return tensor, offsets, non_empty
 
 
 class CaptionHead(BaseHead):
     def __init__(
         self,
         normalize_input: bool = True,
+        novel_grad_only: bool = False,
         ignore_label: int = -100,
     ):
         super().__init__()
         self.normalize_input = normalize_input
+        self.novel_grad_only = novel_grad_only
         self.loss_func = nn.NLLLoss(ignore_index=ignore_label, reduction="none")
 
-    def loss(self, feats, batch_dict):
+    def loss(self, pc: PointCollection, batch_dict):
         caption_infos = batch_dict["caption_infos"]
-        v2p_map = batch_dict["v2p_map"]
-        adapter_feats = feats[v2p_map]
+        adapter_feats = pc.feature_tensor
         if self.normalize_input:
             adapter_feats = nn.functional.normalize(adapter_feats, dim=-1)
 
         caption_type = "caption_view"
         caption_info = caption_infos[caption_type]
+
+        # Extract caption information
+        frame_corr_idx = caption_info["select_image_corr"]
         caption_embed = caption_info["caption_embed"]
+        caption_idx = caption_info["caption_idx"]
+
+        # Compute caption scores
         caption_logits = adapter_feats @ caption_embed.float().T
         caption_scores = nn.LogSoftmax(dim=-1)(caption_logits)
 
-        pooled_scores, real_n_points, if_has_pts = self._forward_given_type_caption_cuda(
-            batch_dict,
-            caption_info,
-            caption_scores,
+        # Convert data to CSR format
+        corr_idx, offsets, non_empty = convert_list_list_tensor_to_tensor(
+            frame_corr_idx, pc.offsets, remove_empty_list=True
         )
 
-        # if some scene don't have suitable image, len(pooled_features) == 0
-        if len(pooled_scores) > 0:
-            pooled_scores = torch.cat(pooled_scores, 0)
-            real_n_points = torch.cat(real_n_points, 0)
-            exist_caption_idx = torch.cat(if_has_pts, 0)
+        # Reduce caption scores
+        rep_caption_scores = caption_scores[corr_idx]
+        reduced_caption_scores = segment_csr(
+            rep_caption_scores,
+            offsets.to(caption_scores.device),
+            reduce="mean",
+        )
 
-            caption_idx = caption_info["caption_idx"]
-            caption_labels = self.prepare_caption_labels(caption_idx, exist_caption_idx)
-            ret_dict = {
-                "caption_output": pooled_scores,
-                "caption_labels": caption_labels,
-                "caption_n_points": real_n_points,
-            }
-        ret_dict["zero_loss"] = 0.0 * caption_scores.sum()
-        return ret_dict
+        loss = self.loss_func(reduced_caption_scores, caption_idx[non_empty])
+        return loss
 
-    @force_fp32(apply_to=("caption_scores"))
-    def _forward_given_type_caption_cuda(self, batch_dict, caption_info, caption_scores):
-        frame_corr_idx = caption_info["select_image_corr"]
-        batch_idx = batch_dict["batch_idxs"]
-        origin_idx = batch_dict["origin_idx"]  # (N, )
-        origin_num_points = batch_dict["pc_count"]
-        num_points = batch_idx.bincount()
-        device = caption_scores.device
-
-        pooled_scores = []
-        if_has_pts = []
-        real_n_points = []
-        for b in range(len(frame_corr_idx)):
-            caption_to_point_mapping = frame_corr_idx[b]
-            N = len(caption_to_point_mapping)
-            D = caption_scores.shape[-1]
-
-            if N == 0:
-                continue
-
-            point_to_origin_mapping = torch.full(
-                (origin_num_points[b],), -1, dtype=torch.long, device=device
-            )
-            point_to_origin_mapping[origin_idx[batch_idx == b]] = torch.arange(
-                num_points[b], dtype=torch.long, device=device
-            )
-
-            caption_idx_bincount = torch.tensor(
-                [0] + [len(idx) for idx in caption_to_point_mapping],
-                dtype=torch.long,
-                device=device,
-            )
-            caption_idx = (
-                torch.arange(
-                    len(caption_idx_bincount), device=device, dtype=torch.long
-                ).repeat_interleave(caption_idx_bincount)
-                - 1
-            )
-
-            if isinstance(caption_to_point_mapping[0], np.ndarray):
-                caption_to_point_mapping = np.concatenate(caption_to_point_mapping, 0)
-                caption_to_point_mapping = (
-                    torch.from_numpy(caption_to_point_mapping).long().to(device)
-                )
-            else:
-                caption_to_point_mapping = torch.cat(caption_to_point_mapping, 0).long().to(device)
-
-            if self.novel_grad_only and "binary_labels" in batch_dict:
-                if batch_dict["binary_labels"].shape[0] != batch_idx.shape[0]:
-                    binary_labels = batch_dict["binary_labels"][batch_dict["v2p_map"]]
-                else:
-                    binary_labels = batch_dict["binary_labels"]
-                base_mask = binary_labels[batch_idx == b] == 1
-            else:
-                base_mask = torch.zeros((batch_idx == b).sum().item())
-            assert caption_scores.dtype == torch.float32
-
-            cur_score = caption_scores[batch_idx == b]
-            if (base_mask > 0).any():
-                cur_score[base_mask] = cur_score[base_mask].detach()
-            invalid = (point_to_origin_mapping == -1).float().unsqueeze(-1) * (-1)
-
-            score_and_count = torch.cat(
-                [
-                    cur_score[point_to_origin_mapping[caption_to_point_mapping]],
-                    invalid[caption_to_point_mapping],
-                ],
-                dim=-1,
-            )
-
-            output_score_and_count = torch.zeros((N, D + 1)).to(caption_scores)
-            # apply sum pooling
-            scatter(
-                score_and_count,
-                caption_idx,
-                dim=0,
-                out=output_score_and_count,
-                reduce="sum",
-            )
-            output_score, output_count = output_score_and_count.split([D, 1], dim=-1)
-            _real_n_points = caption_idx_bincount[1:] + output_count.squeeze()
-
-            denom = torch.zeros_like(_real_n_points)
-            denom[_real_n_points > 0] = 1 / _real_n_points[_real_n_points > 0]
-
-            output_score = output_score * denom.unsqueeze(-1)
-
-            _if_has_pts = _real_n_points > 0
-            pooled_scores.append(output_score[_if_has_pts])
-            real_n_points.append(_real_n_points[_if_has_pts])
-            if_has_pts.append(_if_has_pts)
-
-        return pooled_scores, real_n_points, if_has_pts
-
-    def prepare_caption_labels(self, caption_idx, exist_caption_idx=None):
-        if exist_caption_idx is not None:
-            caption_labels = caption_idx[exist_caption_idx]
-        else:
-            caption_labels = caption_idx
-
-        return caption_labels
-
-    def get_loss(self):
-        caption_loss = 0
-        tb_dict = {}
-        for caption_type in self.forward_ret_dict:
-            if "caption_output" in self.forward_ret_dict[caption_type]:
-                caption_output = self.forward_ret_dict[caption_type]["caption_output"]
-                caption_labels = self.forward_ret_dict[caption_type]["caption_labels"]
-                cur_caption_loss_weight = self.loss_weight
-                cur_caption_loss = (
-                    self.loss_func(caption_output, caption_labels) * cur_caption_loss_weight
-                )
-                if len(cur_caption_loss.shape) > 0:
-                    caption_n_points = self.forward_ret_dict[caption_type]["caption_n_points"]
-                    assert len(cur_caption_loss) == len(caption_n_points)
-                    cur_caption_loss = (
-                        (cur_caption_loss * caption_n_points) / (caption_n_points.sum())
-                    ).sum()
-                tb_dict[caption_type] = cur_caption_loss.item()
-            else:
-                tb_dict[caption_type] = 0.0
-                # if some GPUs don't have loss, some GPUs have loss for backward, the process will stuck
-                cur_caption_loss = self.forward_ret_dict[caption_type]["zero_loss"]
-
-            caption_loss += cur_caption_loss
-
-        return caption_loss, tb_dict
+    def forward(self, pc: PointCollection, data_dict: Dict):
+        return self.loss(pc, data_dict)
--- tests/test_caption_head.py
+++ tests/test_caption_head.py
@@ -1,23 +1,33 @@
-from pathlib import Path
-
+import hydra
 import pytest
 import torch
 import yaml
+from lightning import LightningDataModule
 from omegaconf import OmegaConf
+from warp.convnet.geometry.point_collection import PointCollection
 
-from src.data.regionplc.scannet_dataset import ScanNetDataset
+from src.data.scannet.dataset import ScanNetDataset
 from src.models.heads.caption_head import CaptionHead
 from src.models.regionplc.text_models import build_text_model
 from src.models.regionplc.utils.caption_utils import get_caption_batch
-from src.models.regionplc.vfe import IndoorVFE
+
+text_encoder_str = """text_encoder:
+name: CLIP
+backbone: ViT-B/16
+"""
+
+caption_cfg_str = """
+ENABLED: True
+CAPTION_PATH: caption/caption_detic-template_and_kosmos_125k_iou0.2.json
+IMAGE_CORR_PATH: image_corr/scannet_caption_idx_detic-template_and_kosmos_125k_iou0.2.pkl
+SELECT: ratio
+NUM: 1
+RATIO: 0.2
+SAMPLE: 1
+GATHER_CAPTION: False
+"""
 
 
-def yaml_to_dict(yaml_str: str) -> dict:
-    """Converts a YAML string to a dictionary."""
-    return yaml.safe_load(yaml_str)
-
-
-@pytest.mark.parametrize("split", ["train"])
 def test_caption_head(split: str) -> None:
     """Tests `MNISTDataModule` to verify that it can be downloaded correctly, that the necessary
     attributes were created (e.g., the dataloader objects), and that dtypes and batch sizes
@@ -25,65 +35,43 @@ def test_caption_head(split: str) -> None:
 
     :param batch_size: Batch size of the data to be loaded by the dataloader.
     """
-    data_dir = Path("/workspace/datasets/regionplc").expanduser()
-    project_root = Path(__file__).resolve().parents[1]
-    config_path = "configs/tests/scannet.yaml"
-    with open(project_root / config_path) as f:
-        omega_config_dict = yaml_to_dict(f.read())
-
+    with open("configs/data/regionplc_base15.yaml") as f:
+        omega_config_dict = yaml.safe_load(f.read())
     cfg = OmegaConf.create(omega_config_dict)
-    cfg.data_dir = str(data_dir)
-    cfg.split = split
+    cfg.collate_fn._target_ = "src.data.collate.point_collate_warp_fn"
 
+    cfg.val_dataset = cfg.train_dataset
+    datamodule: LightningDataModule = hydra.utils.instantiate(cfg)
     caption_head = CaptionHead(
         normalize_input=True,
     )
 
     device = torch.device("cuda:0")
-    text_encoder = build_text_model(cfg.text_encoder).to(device)
-    vfe = IndoorVFE()
+    text_encoder_cfg = OmegaConf.create(text_encoder_str)
+    text_encoder = build_text_model(text_encoder_cfg).to(device)
 
-    dataset = ScanNetDataset(
-        data_dir=cfg.data_dir,
-        split=cfg.split,
-        class_names=cfg.class_names,
-        repeat=cfg.repeat,
-        point_range=cfg.point_range,
-        voxel_scale=cfg.voxel_scale,
-        max_npoint=cfg.max_npoint,
-        full_scale=cfg.full_scale,
-        rgb_norm=cfg.rgb_norm,
-        xyz_as_feat=cfg.xyz_as_feat,
-        rgb_as_feat=cfg.rgb_as_feat,
-        min_spatial_shape=cfg.min_spatial_shape,
-        ignore_label=cfg.ignore_label,
-        base_class_idx=cfg.base_class_idx,
-        novel_class_idx=cfg.novel_class_idx,
-        ignore_class_idx=cfg.ignore_class_idx,
-        aug_cfg=cfg.aug_cfg,
-        caption_cfg=cfg.caption_cfg,
-    )
-    assert dataset is not None
+    caption_cfg = OmegaConf.create(caption_cfg_str)
+    datamodule.setup("fit")
+    loader = datamodule.train_dataloader()
 
-    assert len(dataset) > 0
-
-    for i in range(len(dataset)):
-        batch_dict = dataset[i]
+    for i, batch_dict in enumerate(loader):
         assert isinstance(batch_dict, dict)
-        print(batch_dict.keys())
 
         batch_dict = get_caption_batch(
-            cfg.caption_cfg,
+            caption_cfg,
             {},
             batch_dict,
             text_encoder,
             local_rank=0,
         )
-        batch_dict = vfe(batch_dict)
 
-        labels = torch.LongTensor(batch_dict["labels"])
-        rand_feats = torch.randn(labels.shape[0], 512)
-        loss = caption_head.loss(rand_feats, batch_dict)
+        rand_feats = torch.randn(batch_dict["coord"].shape[0], 512)
+        pc = PointCollection(batch_dict["coord"], rand_feats, offsets=batch_dict["offsets"]).to(
+            device
+        )
+
+        loss = caption_head.loss(pc, batch_dict)
+        print(loss)
         if i == 0:
             break
 
