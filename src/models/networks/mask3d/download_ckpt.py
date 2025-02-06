e53443724e231590d23fc5051ae8d54c3d057d7b
Chunghyun Park <p0125ch@postech.ac.kr>
2025-02-06 05:45:18 +0000

    OpenIns3D mask conversion

--- src/models/networks/mask3d/download_ckpt.py
+++ src/models/networks/mask3d/download_ckpt.py
@@ -2,6 +2,11 @@ import os
 from pathlib import Path
 
 import gdown
+from natsort import natsorted
+from tqdm import tqdm
+import torch
+import numpy as np
+
 
 ID_CKPTS = {
     "mask3d-scannet200_val.ckpt": "https://omnomnom.vision.rwth-aachen.de/data/mask3d/checkpoints/scannet200/scannet200_val.ckpt",
@@ -43,6 +48,39 @@ def download_openins3d_masks():
     os.system(f"rm {MASK_DIR / 'openins3d.zip'}")
 
 
+def convert_openins3d_masks():
+    mask_dir = MASK_DIR / "mask3d_scannet200_openins3d"
+    assert mask_dir.exists(), f"Mask directory {mask_dir} does not exist"
+
+    out_dir = MASK_DIR / "mask3d_scannet200_openins3d_converted"
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    masks_files = natsorted(mask_dir.glob("*.pt"))
+    for masks_file in tqdm(masks_files):
+        scene_id = masks_file.stem
+        scene_dir = out_dir / scene_id
+        scene_dir.mkdir(parents=True, exist_ok=True)
+
+        # sparse to dense
+        masks_coo = torch.load(masks_file)
+        masks = masks_coo.to_dense().numpy().T
+
+        # convert to point_indices (flattened)
+        point_indices_all = []
+        lengths_all = []
+        for mask in masks:
+            point_indices = np.where(mask)[0].tolist()
+            point_indices_all.extend(point_indices)
+            lengths_all.append(len(point_indices))
+
+        point_indices_all = np.array(point_indices_all).astype(np.int32)
+        lengths_all = np.array(lengths_all).astype(np.int64)
+
+        # save
+        np.savez(scene_dir / "point_indices.npz", packed=point_indices_all, lengths=lengths_all)
+
+
 if __name__ == "__main__":
-    download_ckpt()
-    download_openins3d_masks()
+    # download_ckpt()
+    # download_openins3d_masks()
+    convert_openins3d_masks()
