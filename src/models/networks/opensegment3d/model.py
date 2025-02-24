91e0cec9cca1a35b011b8307cf49bd5caed045b3
Chunghyun Park <p0125ch@postech.ac.kr>
2025-02-24 11:53:21 +0000

    Add OpenSegment3D, a refactored Segment3D fully written in SpConv

---
+++ src/models/networks/opensegment3d/model.py
@@ -0,0 +1,349 @@
+from typing import List, Optional, Dict
+
+import torch
+import torch.nn as nn
+from torch.cuda.amp import autocast
+from torch_scatter import scatter
+from spconv.pytorch.core import ImplicitGemmIndiceData
+import spconv.pytorch as spconv
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+import pointops
+
+from src.models.networks.segment3d.modules.helpers_3detr import GenericMLP
+from src.models.networks.segment3d.modules.position_embedding import PositionEmbeddingCoordsSine
+from src.models.networks.mask3d.mask3d import CrossAttentionLayer, FFNLayer, SelfAttentionLayer
+from src.models.components.structure import Point
+from src.models.components.misc import batch2offset
+
+
+class OpenSegment3D(nn.Module):
+    def __init__(
+        self,
+        backbone: nn.Module,
+        hidden_dim: int,
+        feedforward_dim: int,
+        clip_dim: int,
+        num_queries: int,
+        num_heads: int,
+        decoder_iterations: int,
+        max_sample_sizes: List[int],
+        hlevels: List[int],
+    ):
+        super().__init__()
+        self.backbone = backbone
+        assert self.backbone.out_fpn, "Backbone must output FPN features"
+
+        self.hidden_dim = hidden_dim
+        self.feedforward_dim = feedforward_dim
+        self.clip_dim = clip_dim
+        self.num_queries = num_queries
+        self.num_heads = num_heads
+        self.decoder_iterations = decoder_iterations
+        self.max_sample_sizes = max_sample_sizes
+        self.hlevels = hlevels
+        self.num_hlevels = len(hlevels)
+
+        backbone_decoder_channels = self.backbone.channels[-5:]
+        self.decoder_proj = nn.Linear(self.backbone.out_channels, hidden_dim)
+        self.query_proj = GenericMLP(
+            input_dim=self.hidden_dim,
+            hidden_dims=[self.hidden_dim],
+            output_dim=self.hidden_dim,
+            use_conv=False,
+            output_use_activation=True,
+            hidden_use_bias=True,
+        )
+
+        # Transformer decoder
+        self.pos_enc = PositionEmbeddingCoordsSine(
+            pos_type="fourier",
+            d_pos=hidden_dim,
+            gauss_scale=1.0,
+            normalize=True,
+        )
+        self.decoder_norm = nn.LayerNorm(hidden_dim)
+
+        self.linear = nn.ModuleList()
+        self.cross_attention = nn.ModuleList()
+        self.self_attention = nn.ModuleList()
+        self.ffn = nn.ModuleList()
+        for hlevel in self.hlevels:  # coarse to fine
+            self.linear.append(nn.Linear(backbone_decoder_channels[hlevel], hidden_dim))
+            self.cross_attention.append(CrossAttentionLayer(d_model=hidden_dim, nhead=num_heads))
+            self.self_attention.append(SelfAttentionLayer(d_model=hidden_dim, nhead=num_heads))
+            self.ffn.append(FFNLayer(d_model=hidden_dim, dim_feedforward=feedforward_dim))
+
+        # Prediction heads
+        self.class_head = nn.Linear(hidden_dim, 1)
+        self.mask_head = nn.Sequential(
+            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), nn.Linear(hidden_dim, hidden_dim)
+        )
+        self.clip_head = nn.Linear(hidden_dim, clip_dim)
+
+        # rearrange layers
+        self.b2q = Rearrange("b q d -> q b d")
+        self.q2b = Rearrange("q b d -> b q d")
+
+    def forward(self, input_dict: dict, num_queries: Optional[int] = None):
+        num_queries = num_queries or self.num_queries
+
+        # backbone
+        point, fpn_stensors = self.backbone(input_dict)
+        max_hlevel = len(fpn_stensors)
+        voxel_indices = point.sparse_conv_feat.indices
+        batch_size = point.sparse_conv_feat.batch_size
+        dtype = point.sparse_conv_feat.features.dtype
+        device = point.sparse_conv_feat.features.device
+        batch_splits_all = self.get_batch_splits(fpn_stensors, batch_size)
+
+        # positional encodings
+        centroids_all = self.get_centroids(point, max_hlevel)
+        pos_encs_all = self.get_positional_encodings(
+            batch_splits_all, centroids_all, dtype, device
+        )
+
+        # point features
+        point_features = self.decoder_proj(fpn_stensors[-1].features)
+        decomposed_pfeats = [point_features[batch_split] for batch_split in batch_splits_all[-1]]
+
+        # query sampling
+        voxel_coords = voxel_indices[:, 1:]
+        batch_size = point.sparse_conv_feat.batch_size
+        offset = batch2offset(voxel_indices[:, 0])
+        new_offset = torch.tensor(
+            [num_queries * (i + 1) for i in range(batch_size)],
+            dtype=torch.long,
+            device=offset.device,
+        )
+        query_indices = pointops.farthest_point_sampling(voxel_coords.float(), offset, new_offset)
+        query_indices = query_indices.long()
+
+        # queries
+        query_pos = pos_encs_all[-1][query_indices]
+        query_pos = self.query_proj(query_pos)
+        query_pos = rearrange(query_pos, "(b q) d -> b q d", q=num_queries)
+        queries = torch.zeros_like(query_pos)
+
+        # decoding
+        for _ in range(self.decoder_iterations):  # shared decoder weights
+            for i, hlevel in enumerate(self.hlevels):  # coarse to fine
+                # (1) mask module
+                attn_mask = self.attention_mask_module(
+                    queries,
+                    decomposed_pfeats,
+                    point.sparse_conv_feat.indice_dict,
+                    max_hlevel - hlevel - 1,
+                )
+
+                # (2) query refinement
+                batch_splits = batch_splits_all[hlevel]
+                fpn_stensor = fpn_stensors[hlevel]
+                pos_encs = pos_encs_all[hlevel]
+
+                decomposed_fpn = [
+                    fpn_stensor.features[batch_split] for batch_split in batch_splits
+                ]
+                decomposed_attn = [attn_mask[batch_split] for batch_split in batch_splits]
+                decomposed_pos_enc = [pos_encs[batch_split] for batch_split in batch_splits]
+
+                # key-value sampling
+                sample_size = max([len(x) for x in decomposed_fpn])
+                if min([len(x) for x in decomposed_fpn]) == 1:
+                    raise RuntimeError("only a single point gives nans in cross-attention")
+
+                if self.training:
+                    sample_size = min(sample_size, self.max_sample_sizes[hlevel])
+
+                indices_all = []
+                masks_all = []
+                for features in decomposed_fpn:
+                    num_voxels = len(features)
+                    if num_voxels <= sample_size:
+                        indices = torch.zeros(sample_size, dtype=torch.long, device=device)
+                        indices[:num_voxels] = torch.arange(num_voxels, device=device)
+                        masks = torch.ones(sample_size, dtype=torch.bool, device=device)
+                        masks[:num_voxels] = False
+                    else:
+                        indices = torch.randperm(num_voxels, device=device)[:sample_size]
+                        masks = torch.zeros(num_voxels, dtype=torch.bool, device=device)
+
+                    indices_all.append(indices)
+                    masks_all.append(masks)
+
+                # batchify
+                batched_fpn = torch.stack(
+                    [features[indices] for features, indices in zip(decomposed_fpn, indices_all)]
+                )
+                batched_attn = torch.stack(
+                    [attn[indices] for attn, indices in zip(decomposed_attn, indices_all)]
+                )
+                batched_pos_enc = torch.stack(
+                    [
+                        pos_encs[indices]
+                        for pos_encs, indices in zip(decomposed_pos_enc, indices_all)
+                    ]
+                )
+
+                batched_attn.permute((0, 2, 1))[batched_attn.sum(1) == sample_size] = False
+                batched_masks = torch.stack(masks_all)
+                batched_attn = torch.logical_or(batched_attn, batched_masks[..., None])
+
+                # transformer decoder
+                batched_attn = repeat(batched_attn, "b q n -> (b h) n q", h=self.num_heads)
+                batched_fpn = self.linear[i](batched_fpn)
+                queries = self.cross_attention[i](
+                    self.b2q(queries),
+                    self.b2q(batched_fpn),
+                    memory_mask=batched_attn,
+                    memory_key_padding_mask=None,
+                    pos=self.b2q(batched_pos_enc),
+                    query_pos=self.b2q(query_pos),
+                )
+                queries = self.self_attention[i](queries, query_pos=self.b2q(query_pos))
+                queries = self.ffn[i](queries)
+                queries = self.q2b(queries)
+
+        pred_classes, pred_masks, pred_clip_feats = self.mask_module(
+            queries,
+            decomposed_pfeats,
+        )
+        return dict(
+            logits=pred_classes,
+            masks=pred_masks,
+            clip_feats=pred_clip_feats,
+        )
+
+    def mask_module(self, queries: torch.Tensor, decomposed_point_feats: List[torch.Tensor]):
+        queries = self.decoder_norm(queries)  # [b, q, d]
+        pred_classes = self.class_head(queries)
+        mask_embeds = self.mask_head(queries)
+        pred_masks = [
+            point_feats @ mask_embed.T
+            for point_feats, mask_embed in zip(decomposed_point_feats, mask_embeds)
+        ]
+        pred_clip_feats = self.clip_head(queries)
+        return pred_classes, pred_masks, pred_clip_feats
+
+    def attention_mask_module(
+        self,
+        queries: torch.Tensor,
+        decomposed_point_feats: List[torch.Tensor],
+        indice_dict: Dict[str, ImplicitGemmIndiceData],
+        num_pooling_steps: int,
+    ):
+        assert num_pooling_steps > 0
+
+        queries = self.decoder_norm(queries)
+        mask_embeds = self.mask_head(queries)
+        pred_masks = [
+            point_feats @ mask_embed.T
+            for point_feats, mask_embed in zip(decomposed_point_feats, mask_embeds)
+        ]
+        attn_masks = torch.vstack(pred_masks)
+        for i in range(num_pooling_steps):
+            indice_data = indice_dict[f"spconv{i + 1}"]
+            attn_masks, _ = spconv.ops.indice_avgpool_implicit_gemm(
+                attn_masks, indice_data.pair_fwd, indice_data.pair_fwd.shape[1], False
+            )
+
+        return attn_masks
+
+    def get_batch_splits(self, stensors: List[spconv.SparseConvTensor], batch_size: int):
+        batch_splits_all = []
+        for stensor in stensors:
+            indices = stensor.indices
+            batch_splits = []
+            for i in range(batch_size):
+                mask = indices[:, 0] == i
+                batch_splits.append(mask)
+            batch_splits_all.append(batch_splits)
+
+        return batch_splits_all
+
+    def get_centroids(self, point: Point, max_hlevel: int):
+        centroids = scatter(point.coord, point.v2p_map, dim=0, reduce="mean")
+        indice_dict = point.sparse_conv_feat.indice_dict
+
+        with torch.no_grad():
+            centroids_all = [centroids]
+            for i in range(max_hlevel - 1):
+                indice_data = indice_dict[f"spconv{i + 1}"]
+                centroids_down, _ = spconv.ops.indice_avgpool_implicit_gemm(
+                    centroids_all[i], indice_data.pair_fwd, indice_data.pair_fwd.shape[1], False
+                )
+                centroids_all.append(centroids_down)
+            centroids_all.reverse()  # coarse to fine
+
+        return centroids_all
+
+    def get_positional_encodings(
+        self,
+        batch_splits_all: List[List[torch.Tensor]],
+        centroids_all: List[torch.Tensor],
+        dtype: torch.dtype,
+        device: torch.device,
+    ):
+        pos_encs_all = []
+        for batch_splits, centroids in zip(batch_splits_all, centroids_all):
+            num_voxels = len(centroids)
+
+            pos_encs = torch.zeros(num_voxels, self.hidden_dim, dtype=dtype, device=device)
+            for batch_split in batch_splits:
+                masked_centroids = centroids[batch_split]
+                scene_bounds = [
+                    masked_centroids.min(dim=0, keepdim=True).values,
+                    masked_centroids.max(dim=0, keepdim=True).values,
+                ]
+
+                with autocast(enabled=False):
+                    pos_enc = self.pos_enc(
+                        masked_centroids[None, ...].float(), input_range=scene_bounds
+                    )
+
+                pos_enc = rearrange(pos_enc, "1 d n -> n d")
+                pos_encs[batch_split] = pos_enc
+
+            pos_encs_all.append(pos_encs)
+
+        return pos_encs_all
+
+
+if __name__ == "__main__":
+    from src.models.networks.spunet.spconv_unet_v1m1_base import SpUNetBase
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    backbone = SpUNetBase(
+        in_channels=3,
+        out_channels=512,
+        base_channels=32,
+        channels=[32, 64, 128, 256, 256, 128, 96, 96],
+        layers=[2, 2, 2, 2, 2, 2, 2, 2],
+        out_fpn=True,
+        hash_method="ravel",
+    )
+    model = OpenSegment3D(
+        backbone=backbone,
+        hidden_dim=128,
+        feedforward_dim=1024,
+        clip_dim=512,
+        num_queries=400,
+        num_heads=8,
+        decoder_iterations=3,
+        max_sample_sizes=[200, 800, 3200, 12800, 51200],
+        hlevels=[0, 1, 2, 3],
+    ).to(device)
+    print(">>> OpenSegment3D initialized")
+
+    input_dict = {
+        "grid_size": 0.02,
+        "coord": torch.rand(10000, 3, device=device),
+        "offset": torch.tensor([4000, 10000], dtype=torch.long, device=device),
+        "feat": torch.rand(10000, 3, device=device),
+    }
+    pred_classes, pred_masks = model(input_dict)
+    print(">>> Forward passed")
+    print("    pred_classes.shape:", pred_classes.shape)
+    print("    len(pred_masks):", len(pred_masks))
+    print("    pred_masks[0].shape:", pred_masks[0].shape)
